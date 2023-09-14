7f4e3034c724ec97160fbc756132d2bc021e35c4
Lucaspaz21 <60491853+Lucaspaz21@users.noreply.github.com>
2023-09-13 23:13:30 -0300

    Add files via upload

---
+++ lsp/defs.py
@@ -0,0 +1,28 @@
+def oito_furos_em_pe(area):
+    bloco_por_mt = 25
+    qtd = (area * bloco_por_mt)
+    return int(qtd)
+
+
+def oito_furos_deitado(area):
+    bloco_por_mt = 50
+    qtd = area * bloco_por_mt
+    return int(qtd)
+
+
+def seis_furos_em_pe(area):
+    bloco_por_mt = 27
+    qtd = area * bloco_por_mt
+    return int(qtd)
+
+
+def seis_furos_deitado(area):
+    bloco_por_mt = 32
+    qtd = area * bloco_por_mt
+    return int(qtd)
+
+
+def tijolo_baiano(area):
+    tijolo_por_mt = 83
+    qtd = area * tijolo_por_mt
+    return int(qtd)
